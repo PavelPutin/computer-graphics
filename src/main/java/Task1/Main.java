319e6a92950a8c21bd4bde558501d778510e46ef
Putin Pavel <putin@cs.vsu.ru>
2022-09-14 14:18:22 +0300

    Добавил главное окно

--- src/main/java/Task1/Main.java
+++ src/main/java/Task1/Main.java
@@ -1,7 +1,12 @@
 package Task1;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Happy, birthday!");
+        MainFrame mf = new MainFrame();
+        mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mf.setSize(800, 600);
+        mf.setVisible(true);
     }
 }
---
+++ src/main/java/Task1/MainFrame.java
@@ -0,0 +1,6 @@
+package Task1;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+}
