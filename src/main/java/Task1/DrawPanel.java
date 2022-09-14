22664d9fe9888b4fa3e522365ccce40d99a5b4aa
Putin Pavel <putin@cs.vsu.ru>
2022-09-14 14:32:17 +0300

    Создана DrawPanel для вывода графической информации

---
+++ src/main/java/Task1/DrawPanel.java
@@ -0,0 +1,14 @@
+package Task1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+    public DrawPanel() {
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
--- src/main/java/Task1/MainFrame.java
+++ src/main/java/Task1/MainFrame.java
@@ -1,6 +1,13 @@
 package Task1;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private final DrawPanel dp;
+
+    public MainFrame() throws HeadlessException {
+        dp = new DrawPanel();
+        this.add(dp);
+    }
 }
